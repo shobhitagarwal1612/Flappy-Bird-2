76dc8f040bf294e55750c5dafa95bc2b009f7734
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2019-01-30 23:25:15 +0530

    Add a basic rotating and scaling animation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import cocos
+from cocos.actions import ScaleBy, Repeat, Reverse, RotateBy
 
 
-class HelloWorld(cocos.layer.Layer):
+class HelloWorld(cocos.layer.ColorLayer):
 
     def __init__(self):
-        super(HelloWorld, self).__init__()
+        super(HelloWorld, self).__init__(64, 64, 224, 255)
 
         label = cocos.text.Label(
             'Hello, world',
@@ -17,10 +18,25 @@ class HelloWorld(cocos.layer.Layer):
 
         self.add(label)
 
+        sprite = cocos.sprite.Sprite('res/HelloWorld.png')
+        sprite.position = 320, 240
+        sprite.scale = 3
+        self.add(sprite, z=1)
+
+        scale = ScaleBy(3, duration=2)
+
+        label.do(Repeat(scale + Reverse(scale)))
+        sprite.do(Repeat(Reverse(scale) + scale))
+
 
 cocos.director.director.init()
 
 hello_layer = HelloWorld()
+
+hello_layer.do(RotateBy(360, duration=10))
+
+# A scene that contains the layer hello_layer
 main_scene = cocos.scene.Scene(hello_layer)
 
+# And now, start the application, starting with main_scene
 cocos.director.director.run(main_scene)
